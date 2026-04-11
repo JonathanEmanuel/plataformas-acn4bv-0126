ae6beaa38050f7ef9176c74095db5e97ff68966e
Jonathan Cruz <cruzjonathan.e@gmail.com>
2026-04-10 21:43:00 -0300

    Semana 04 - DOM y Eventos

---
+++ semana-04/script.js
@@ -0,0 +1,18 @@
+const lista = ['uno', 'dos', true, ['a', 'b', 'c']];
+// Objetos literales
+const persona = {
+    nombre: 'Julian',
+    email: 'jualian@dv.edu.ar',
+    cursos: ['HTML', 'CSS', 'JavaScript']
+}
+
+console.log( persona.nombre );
+/*
+const titulo = document.getElementsByTagName('h1');
+const textoImportante = document.getElementsByClassName('.textImportante');
+const textoUnico = document.getElementById('#textoUnico');
+*/
+
+const titulo = document.querySelector('h1');
+const textoImportante = document.querySelector('.textImportante');
+const textoUnico = document.querySelector('#textoUnico');
