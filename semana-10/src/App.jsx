eed167c0c38994c6366f9c245eb989f6c6efae84
Jonathan Cruz <cruzjonathan.e@gmail.com>
2026-05-22 21:36:48 -0300

    Semana 10 - Props y Estados

---
+++ semana-10/src/App.jsx
@@ -0,0 +1,19 @@
+import heroImg from './assets/hero.png'
+import './App.css'
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <h1>Props</h1>
+
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ semana-10/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='bg-dark text-white d-flex justify-content-center align-items-center'>
+        <h1> Props y Estados</h1>
+    </header>
+  )
+}
+
+export default Header
