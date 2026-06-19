c3ebe876b9ce2b233e16bf9fb6156c70d500313e
Jonathan Cruz <cruzjonathan.e@gmail.com>
2026-06-19 20:25:57 -0300

    Semana 14 - User

--- semana-14/backend/index.js
+++ semana-14/backend/index.js
@@ -1,8 +1,12 @@
 import connectDB from './config/db.js';
 import cors from 'cors';
-import express from 'express'
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import express from 'express';
 import { productModel } from './models/productModel.js';
+import { userModel } from './models/userModel.js';
 const PORT = 5000;
+const SECRET_KEY = 'claveSecreta';
 const app = express();
 app.use( express.json());
 app.use( cors());
@@ -89,6 +93,89 @@ app.put( '/api/products/:uid', async (req, res) => {
         console.error(error);
     }
 })
+
+/* USER */
+app.post('/api/users/register', async (req, res) => {
+    try {
+
+        const { name, email, password } = req.body;
+        const existUser = await userModel.findOne({ email });
+        if (existUser) {
+            return res.status(400).json({
+                status: 'error',
+                message: 'El usuario ya existe'
+            });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new userModel({
+            name,
+            email,
+            password: hashedPassword
+        });
+        const user = await newUser.save();
+        res.status(201).json({
+            status: 'success',
+            data: user
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            status: 'error'
+        });
+    }
+});
+
+app.post('/api/users/login', async (req, res) => {
+    try {
+
+        const { email, password } = req.body;
+
+        const user = await userModel.findOne({ email });
+
+        if (!user) {
+            return res.status(401).json({
+                status: 'error',
+                message: 'Credenciales inválidas'
+            });
+        }
+
+        const isValidPassword = await bcrypt.compare(
+            password,
+            user.password
+        );
+
+        if (!isValidPassword) {
+            return res.status(401).json({
+                status: 'error',
+                message: 'Credenciales inválidas'
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                id: user._id,
+                email: user.email
+            },
+            SECRET_KEY,
+            {
+                expiresIn: '1h'
+            }
+        );
+
+        res.json({
+            status: 'success',
+            token
+        });
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            status: 'error'
+        });
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Servidor Web en puerto ${PORT}`);
